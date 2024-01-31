16bfc0a77f4c08c7a928e616fa26c52249c33c4f
joao <rjoao.pedro05@gmail.com>
2024-01-30 20:46:14 -0400

    first

---
+++ SnakeGame/src/snakegame/GameFrame.java
@@ -0,0 +1,17 @@
+package snakegame;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+	GameFrame(){
+		GamePanel panel = new GamePanel();
+		
+		this.add(panel);
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(true);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
